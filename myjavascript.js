e0ab1cefcca2d80a54a9d436b11871f278c50c13
muskaanmittal129 <muskaanmittal129@gmail.com>
2019-08-26 15:47:41 +0530

    position is given

---
+++ myjavascript.js
@@ -0,0 +1,108 @@
+var arr = new Array(8); //create 1-d array
+//create 2d array using 1d array
+for(var i = 0; i<arr.length; i++){
+    arr[i]=new Array(8);
+}
+// Loop to initilize 2D array elements. 
+for (var i = 0; i < 8; i++) { 
+    for (var j = 0; j < 8; j++) { 
+        arr[i][j] = 0; 
+    } 
+} 
+
+
+var c = document.getElementById("mycanvas");
+mycanvas.width = window.innerWidth;
+mycanvas.height = window.innerHeight;
+var ctx= c.getContext("2d");
+
+
+
+
+
+function gameboard(){
+    console.log("a");
+    var rows = 8;
+    var cols = 8;
+    for(var i = 0; i<cols; i++){
+        for(var j = 0; j<rows; j++){
+            console.log("d");
+            var x = i*80;
+            var y =j*80;
+            ctx.rect(x,y,80,80);
+            ctx.stroke();
+           
+            
+    
+            
+        }
+    }}
+
+
+
+
+
+var turn = 1;
+var cursorX, cursorY;
+document.onmousedown = function(e){
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+    console.log(cursorX);
+    console.log(cursorY)
+   // var q = Math.floor(cursorX/80);
+
+var p = Math.floor(cursorX/80);
+var q = Math.floor(cursorY/80);
+if(turn == 1 && arr[p][q]<5){
+arr[p][q]+=1;
+drawcircle();}
+else if (turn==2){
+    if(arr[p][q]==0)
+    {
+        arr[p][q]=5;
+        drawcircle();
+    }
+    else if (arr[p][q]>=5){
+        arr[p][q]+=1;
+        drawcircle();
+    }
+}
+console.log(arr);
+function drawcircle(){
+
+ctx.beginPath();
+if(arr[p][q]==1 || arr[p][q]==5){
+    ctx.arc(p*80+40,q*80+40,20,0,2*Math.PI);
+}
+if(arr[p][q]==2 ||arr[p][q]==6){
+    ctx.arc(p*80+50,q*80+50,20,0,2*Math.PI);
+    ctx.beginPath();
+    ctx.arc(p*80+30,q*80+30,20,0,2*Math.PI);
+}
+if(arr[p][q]==3 ||arr[p][q]==7){
+    ctx.arc(p*80+60,q*80+50,20,0,2*Math.PI);
+    ctx.beginPath();
+    ctx.arc(p*80+40,q*80+40,20,0,2*Math.PI);
+    ctx.beginPath();
+    ctx.arc(p*80+30,q*80+40,20,0,2*Math.PI);
+}
+if(turn==1){
+    ctx.fillStyle = "red";
+
+}
+else{
+    ctx.fillStyle = "blue";
+}
+ctx.fill();
+ctx.stroke();
+console.log("g");
+if(turn==1)
+turn=2;
+else
+turn=1;
+
+}
+
+
+}
+
